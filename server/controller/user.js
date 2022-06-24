63394fce7556b3d1f0e412e23e92d0a7225e38cb
Adityagautamm <adityagautamm@gmail.com>
2022-06-24 20:32:29 +0530

    API code updated

--- server/controller/user.js
+++ server/controller/user.js
@@ -27,25 +27,26 @@ export const signin = async (req, res) => {
 }
 
 export const signup = async (req, res) => {
-    const { firstName, lastName, email, password, confirmPassoword } = req.body;
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
 
     try {
         const existingUser = await userModel.findOne({ email });
 
         if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
-        if (password !== confirmPassoword) return res.status(400).body({ message: 'password doesnt match' });
+        if (password !== confirmPassword) return res.status(401).json({ message: 'password doesnt match' });
 
-        const hashedPassword = bcrypt.hash(password, 12);
+        const hashedPassword = await bcrypt.hash(password, 12);
 
-        const result = userModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+        const result = await userModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
         const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" })
 
 
         res.status(200).json({ result, token });
     } catch (error) {
-        res.status(500).json({ message: "Something went wrong" });
+        res.status(500).json({ message: "Something went wronggggggggggggg" });
+        console.error(error.response.data);
     }
 
 
